905b8d4acda7c869d039f257bce4e524bb6943aa
Jakob Runge <sicarius@g4t3.de>
2019-03-22 16:52:59 +0100

    Remove exampleDesign

--- index.js
+++ index.js
@@ -10,14 +10,6 @@ const {
   API_SECRET: apiSecret,
 } = process.env;
 
-const exampleDesign = {
-  name: '@sicarius',
-  description: 'Avatar of @sicarius',
-  // sourceUrl: 'https://avatars0.githubusercontent.com/u/5417642?s=460&v=4',
-  sourceUrl: 'https://image.spreadshirtmedia.net/image-server/v1/configurations/16940335.jpg',
-  price: 1,
-};
-
 (async () => {
   const session = await api.createSecuritySession(username, password);
   console.log('Logged in as', sessionUtils.userId(session));
