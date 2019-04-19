8cb464eb22a29314f427500da802a88ffd3b714f
Jakob Runge <sicarius@g4t3.de>
2019-04-19 18:39:08 +0200

    Draft setComission

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const fetch = require('node-fetch');
 const Readable = require('stream').Readable;
+const compareDest = require('date-fns/compare_desc');
 
 const { createAuthorizedFetch, withCookies, withDebug } = require('./authorized');
 
@@ -56,7 +57,7 @@ function findIdForIsoCodeInCurrenciesData(currenciesData, wantedIsoCode = 'EUR')
   return currency ? currency.id : null;
 }
 
-async function fetchIdeas(doFetch) {
+async function fetchIdeas(doFetch, userId) {
   const url = `${apiBaseUrl}/users/${userId}/ideas?fullData=true&mediaType=json&currencyId=1&locale=de_DE&offset=0&limit=47`;
 
   const ideasResponse = await doFetch(url, {method: 'GET'});
@@ -64,12 +65,26 @@ async function fetchIdeas(doFetch) {
   return ideasResponse.json();
 }
 
-function toBase64(input) {
-  return Buffer.from(input).toString('base64');
+function newestIdea(ideas) {
+  return ideas.list.reduce(
+    (last, next) => {
+      if (last === null) {
+        return next;
+      }
+
+      const cmp = compareDest(
+        new Date(last.dateCreated),
+        new Date(next.dateCreated),
+      );
+
+      return (cmp < 0) ? last : next;
+    },
+    null,
+  );
 }
 
-function fromBase64(input) {
-  return Buffer.from(input, 'base64').toString('utf8');
+function toBase64(input) {
+  return Buffer.from(input).toString('base64');
 }
 
 function asyncStat(filePath) {
@@ -123,21 +138,44 @@ async function patchIdea(doFetch, createResponse, filePath) {
   return patchResponse;
 }
 
+async function setCommission(doFetch, idea, amount, currency = 'EUR') {
+  const { href, ...rest } = idea;
+  const ideaWithCommission = {
+    ...rest,
+    commission: {
+      amount,
+      currencyId: '1',
+    },
+  };
+
+  const response = await doFetch(href, {method: 'PUT'});
+
+  return response.json();
+}
+
 (async () => {
   const {id: sessionId, user: {id: userId}} = await createSession(fetch);
   const filePath = './example.png';
-  console.log('created Session', {sessionId, userId});
 
-  const authorizedFetch = createAuthorizedFetch(withCookies(withDebug(fetch)), sessionId, apiKey, apiSecret);
+  const authorizedFetch = createAuthorizedFetch(withCookies(fetch), sessionId, apiKey, apiSecret);
 
+  // Need to fetch state to obtain session cookie
   const state = await fetchState(authorizedFetch, userId);
-  console.log('state', state);
 
-  const creation = await createIdea(authorizedFetch, userId, filePath);
-  console.log('creation', creation, creation.headers.raw());
+  // const creation = await createIdea(authorizedFetch, userId, filePath);
+  // console.log('creation', creation);
+  // console.log('creation.headers', creation.headers.raw());
+  // console.log('creation.text', await creation.text())
 
-  const patch = await patchIdea(authorizedFetch, creation, filePath);
-  console.log('patch', patch);
-  console.log('patch.headers', patch.headers.raw());
-  console.log('patch.text', await patch.text());
+  // const patch = await patchIdea(authorizedFetch, creation, filePath);
+  // console.log('patch', patch);
+  // console.log('patch.headers', patch.headers.raw());
+  // console.log('patch.text', await patch.text());
+
+  const ideas = await fetchIdeas(authorizedFetch, userId);
+  const newest = newestIdea(ideas);
+  console.log('newest', JSON.stringify(newest, undefined, 2));
+
+  const withCommision = await setCommission(authorizedFetch, newest, 5);
+  console.log('withCommission', JSON.stringify(withCommision, undefined, 2));
 })();
